9bf43d03f7e31910d1c77297b0459509a82eda4c
ryanraitz <46646466+ryanraitz@users.noreply.github.com>
2019-09-22 23:52:19 -0400

    Add files via upload

---
+++ HelloWorld.scala
@@ -0,0 +1,10 @@
+// Program to print Hello World!  
+object HelloWorld
+{ 
+    // Main Method  
+    def main(args: Array[String])  
+    { 
+        // print phrase 
+        println("Hello World!")  
+    } 
+} 
